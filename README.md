0495f38a9c7f85b02baf7c23d607ee76c50a4527
mac <mac@macui-MacBook-Pro.local>
2019-04-28 13:16:25 +0900

    I can upload Image by URL

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Meme Generator App
 
-- [ ] Upload an image that will appear in a canvas
+- [x] Upload an image that will appear in a canvas
 - [ ] Add text in the top part of the image
 - [ ] Add text in the bottom part of the image
 - [ ] Select the color of the text
 - [ ] Select the size of the text
 - [ ] Save the resulting meme
+
+#### Memo
+
+.populate("ref 콜렉션", ["원하는 필드"])
--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const uploadBtn = document.getElementById("jsUpload");
+const input = document.getElementById("jsInput");
+const formTop = document.getElementById("jsForm__top");
+const formBot = document.getElementById("jsForm__bot");
 const img = new Image();
-img.src =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Kakao_logo.jpg/220px-Kakao_logo.jpg";
 
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
+const handleDraw = img => {
+  ctx.drawImage(img, 0, 0, ctx.canvas.width, ctx.canvas.height);
+};
+
 const handleUpload = () => {
-  const e = ctx.drawImage(img, 0, 0, ctx.canvas.width, ctx.canvas.height);
+  const inputValue = input.value;
+  img.src = inputValue;
+  handleDraw(img);
+  input.value = "";
+};
+
+const handleTopText = () => {
+  event.preventDefault();
+  console.log("top");
+};
+
+const handleBotText = () => {
+  event.preventDefault();
+  console.log("bot");
 };
 
 function init() {
   uploadBtn.addEventListener("click", handleUpload);
+  formTop.addEventListener("submit", handleTopText);
+  formBot.addEventListener("submit", handleBotText);
 }
 
 if (canvas) {
