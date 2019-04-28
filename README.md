6e59041320d3dbf6aad11f0fbf0da3de67a54fb3
mac <mac@macui-MacBook-Pro.local>
2019-04-28 13:45:39 +0900

    Add text in the canvas

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Meme Generator App
 
 - [x] Upload an image that will appear in a canvas
-- [ ] Add text in the top part of the image
-- [ ] Add text in the bottom part of the image
+- [x] Add text in the top part of the image
+- [x] Add text in the bottom part of the image
 - [ ] Select the color of the text
 - [ ] Select the size of the text
 - [ ] Save the resulting meme
--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const uploadBtn = document.getElementById("jsUpload");
 const input = document.getElementById("jsInput");
+const topInput = document.getElementById("jsTopInput");
+const botInput = document.getElementById("jsBotInput");
 const formTop = document.getElementById("jsForm__top");
 const formBot = document.getElementById("jsForm__bot");
 const img = new Image();
 
 ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
+
+ctx.font = "30px Comic Sans MS";
+ctx.fillStyle = "black";
+ctx.textAlign = "center";
 
 const handleDraw = img => {
   ctx.drawImage(img, 0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -22,12 +28,14 @@ const handleUpload = () => {
 
 const handleTopText = () => {
   event.preventDefault();
-  console.log("top");
+  let topValue = topInput.value;
+  ctx.strokeText(topValue, 350, 50);
 };
 
 const handleBotText = () => {
   event.preventDefault();
-  console.log("bot");
+  let botValue = botInput.value;
+  ctx.strokeText(botValue, 350, 600);
 };
 
 function init() {
